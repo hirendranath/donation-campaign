168807698bbc51a429ee1dfdd3579920aea628ea
hirendranath <contact.hirendranath@gmail.com>
2023-09-25 12:30:37 +0600

    JSON data created for use

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
     return (
         <div>
-           <div className="flex justify-between items-center my-5 px-20">
+           <div className="flex justify-between px-24 my-4 items-center ">
                 <div>
                     <img src="./Logo.png" alt="" />
                 </div>
-                <ul className="flex gap-4 font-bold">
+                <ul className="flex gap-4 font-semibold">
                     <li><NavLink className={({ isActive,isPending})=>isPending? "pending" :isActive ? "text-red-600 underline" : ""} to='/'>Home</NavLink></li>
                     <li><NavLink className={({ isActive,isPending})=>isPending? "pending" :isActive ? "text-red-600 underline" : ""} to='/donation'>Donation</NavLink></li>
                     <li><NavLink className={({ isActive,isPending})=>isPending? "pending" :isActive ? "text-red-600 underline" : ""} to='/statistics'>Statistics</NavLink></li>
