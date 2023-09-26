79bdb58091428bc0fdf20e5ef8c27d4d126a3c48
hirendranath <contact.hirendranath@gmail.com>
2023-09-26 12:15:37 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import MainRoot from './Components/MainRoot/MainRoot';
-import Home from './Components/Home/Home';
-import DonationDetails from './Components/DonationDetails/DonationDetails';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+import DonationPage from "./pages/DonationPage.jsx";
+import StatisticsPage from "./pages/StatisticsPage.jsx";
+import Details from "./pages/Details.jsx";
 
-const router=createBrowserRouter([
-    {
-      path:'/',
-      element: <MainRoot></MainRoot>,
-      children:[
-        {
-          path:'/',
-          element: <Home></Home>,
-        },
-        {
-          path:'/donation',
-          element: <div>Donation Page</div>,
-        },
-        {
-          path:'/donation/:id',
-          element: <DonationDetails></DonationDetails>,
-          loader:()=>fetch('../data.json')
-        },
-        {
-          path:'/statistics',
-          element: <div>Please Donate me</div>,
-        },
-      ]
-    }
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/donations",
+        element: <DonationPage />,
+        loader: () => fetch("../../donation.json"),
+      },
+      {
+        path: "/statistics",
+        element: <StatisticsPage />,
+      },
+      {
+        path: "/donation/details/:id",
+        element: <Details />,
+      },
+    ],
+  },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.Fragment>
+    <RouterProvider router={router} />
+  </React.Fragment>
+);
